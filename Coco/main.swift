99772f3dac8d3a728cb827fac7977bc7e7b7949b
Michael Griebling <bob@c-inspirations.com>
2015-08-19 06:05:57 -0400

    Initial Commit

---
+++ Coco/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Coco
+//
+//  Created by Mike Griebling on 19 Aug 2015.
+//  Copyright © 2015 Computer Inspirations. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
